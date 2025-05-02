37dbdadd98f84c1f98807cf5e95eeee89e6ab615
leolbl <leo.ibarralopez@gmail.com>
2025-05-02 09:46:19 -0400

    style: header y navbar completos
    Responsive

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,19 +4,24 @@ import './NavBar.css'; // Puedes crear este archivo para estilos opcionales
 export default function NavBar() {
   return (
     <nav className="navbar">
+      
+    <div class="background"></div> 
       <ul>
-        <li><Link to="/">Inicio</Link></li>
-        <li><Link to="/biblio">Biblioteca</Link></li>
-        <li><Link to="/contact">Contacto</Link></li>
-        <li><Link to="/forum">Foro</Link></li>
-        <li><Link to="/aboutus">Sobre Nosotros</Link></li>
-        <li><Link to="/mycollection">Mi Colección</Link></li>
-        <li><Link to="/biblio-manager">Gestión de Biblioteca</Link></li>
-        <li><Link to="/user-manager">Gestión de Usuarios</Link></li>
-        <li><Link to="/forum-manager">Gestión de Foro</Link></li>
-        <li><Link to="/add-content">Añadir Contenido</Link></li>
-        <li><Link to="/historical-events">Eventos Históricos</Link></li>
+      <li><Link to="/aboutus">NOSOTROS</Link></li>
+
+        <li><Link to="/forum">FOROS</Link></li>
+        <li><Link to="/">INICIO</Link></li>
+        <li><Link to="/biblio">BIBLIOTECA</Link></li>
+        <li><Link to="/contact">CONTACTO</Link></li>
+
       </ul>
     </nav>
+
   );
 }
+/*<li><Link to="/mycollection">Mi Colección</Link></li>
+<li><Link to="/biblio-manager">Gestión de Biblioteca</Link></li>
+<li><Link to="/user-manager">Gestión de Usuarios</Link></li>
+<li><Link to="/forum-manager">Gestión de Foro</Link></li>
+<li><Link to="/add-content">Añadir Contenido</Link></li>
+<li><Link to="/historical-events">Eventos Históricos</Link></li>*/
